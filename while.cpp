56d1a56097a7802e249384089075b68968ad7578
prachitirole <93914944+prachitirol@users.noreply.github.com>
2023-07-09 15:41:29 +0530

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   int sum=0, num=1;
+  while (num<=n)
+  {
+    sum=sum+num;
+    cout<<"sum of n numbers:"<< sum<<endl;
+    num=num+1;
+    /* code */
+  }
+  
+}
