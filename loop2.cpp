0930f5478d85202ecae66ff83958de42d5e3c51c
prachitirole <93914944+prachitirol@users.noreply.github.com>
2023-07-18 17:22:57 +0530

    Add files via upload

---
+++ loop2.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+   /* int a;
+    cin>>a;
+    cout<<"enter the value of a :"<< a << endl;
+   if ( a<0)
+{
+    cout <<"a is negative";
+    }
+else 
+    cout<<"a is positive";
+
+*/
+/*int a,b;
+cin>>a;
+cout<<"enter the value of a :"<< a << endl;
+cin>>b;
+cout<<"enter the value of b :"<< b << endl;
+if (a>b)
+{
+    cout<<"a is greater";
+}
+if(b>a){
+    cout<<"b is greater";
+}*/
+int a;
+cin >> a;
+cout<< "enter the value of a:";
+if (a>0){
+    cout << "a is positive";
+
+}
+if(a<0)
+{
+cout << "a is negative";
+
+
+}
+ if(a=0)
+{
+cout << "a is zero";
+
+
+}
+
+
+
+
+
+ }
