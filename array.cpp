5b54a5541ca9470fb0711fc2714e4dcdcb4ef2dc
prachitirole <93914944+prachitirol@users.noreply.github.com>
2023-07-23 15:59:31 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //arrray declaration
+int arr[5]={1,2,3,4,5};
+
+    cout<<"hello joo";
+    int arr1[16]={1};
+   int n=16;
+   for (int i = 0; i < n; i++)
+   {
+     cout<<arr1[i]<<" ";/* code */
+   }
+   int d[9];
+   int n1=9;
+   for (int i = 0; i < n1; i++)
+   {
+     cout<<"my name is prachi"<<" ";
+     cout<<endl;
+     cout<<"hii"<<endl;/* code */
+   }
+   
+
+}
