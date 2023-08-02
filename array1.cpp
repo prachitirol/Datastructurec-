6f2b7a6c2e842a07cdfbc405769922aa28b7498b
prachitirole <93914944+prachitirol@users.noreply.github.com>
2023-08-02 20:59:30 +0530

    some change

---
+++ array1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //arrray declaration
+int arr[5]={1,2,3,4,5};
+
+    cout<<"hello joo";
+    int arr1[16]={1};
+   int n=16;
+   for (int i = 0; i < n; i++)
+   {
+     cout<<arr1[i]<<" ";/* code */
+   }
+   int d[9];
+   int n1=9;
+   for (int i = 0; i < n1; i++)
+   {
+     cout<<"my name is prachi"<<" ";
+     cout<<endl;
+     cout<<"hii"<<endl;/* code */
+   }
+   
+int b[15];
+for (int i = 0; i < b[14]; i++)
+{
+ cout<<"the no. series :"<<b<<endl; /* code */
+}
+
+}
